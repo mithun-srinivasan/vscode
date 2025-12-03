1e8319110830e02a1c81fc8cb4a1387894bc247c
Mithun Srinivasan <omsaimithun@gmail.com>
2025-12-03 15:07:13 +0530

    Add basic demo and addition functionality
    - Created a Demo class with a main method that prints a greeting.
    - Implemented an add class that takes two integers as input from the user and prints their sum.

---
+++ add.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+class add {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int a, b, sum;
+        System.out.println("enter a");
+        a = sc.nextInt();
+        System.out.println("enter b");
+        b = sc.nextInt();
+        sum = a + b;
+        System.out.println("the addition of " + a + " and " + b + " is " + sum);
+    }
+}

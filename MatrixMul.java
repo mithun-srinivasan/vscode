9c5b4b53ae8d045da7162eee444efa851b1a9a60
Mithun Srinivasan <omsaimithun@gmail.com>
2025-12-12 23:48:26 +0530

    Refactor MatrixMul class to use try-with-resources for Scanner initialization

--- MatrixMul.java
+++ MatrixMul.java
@@ -2,59 +2,60 @@ import java.util.Scanner;
 class MatrixMul {
     @SuppressWarnings("ConvertToTryWithResources")
     public static void main(String args[]) {
-        Scanner sc = new Scanner(System.in);
-        int r1, c1, r2, c2;
+        try (Scanner sc = new Scanner(System.in)) {
+            int r1, c1, r2, c2;
 
-        System.out.println("Enter number of rows and columns for first matrix:");
-        r1 = sc.nextInt();
-        c1 = sc.nextInt();
+            System.out.println("Enter number of rows and columns for first matrix:");
+            r1 = sc.nextInt();
+            c1 = sc.nextInt();
 
-        System.out.println("Enter number of rows and columns for second matrix:");
-        r2 = sc.nextInt();
-        c2 = sc.nextInt();
+            System.out.println("Enter number of rows and columns for second matrix:");
+            r2 = sc.nextInt();
+            c2 = sc.nextInt();
 
-        if (c1 != r2) {
-            System.out.println("Matrix multiplication not possible");
-            sc.close();
-            return;
-        }
-
-        int[][] matrix1 = new int[r1][c1];
-        int[][] matrix2 = new int[r2][c2];
-        int[][] result = new int[r1][c2];
-
-        System.out.println("Enter elements of first matrix:");
-        for (int i = 0; i < r1; i++) {
-            for (int j = 0; j < c1; j++) {
-                matrix1[i][j] = sc.nextInt();
+            if (c1 != r2) {
+                System.out.println("Matrix multiplication not possible");
+                sc.close();
+                return;
             }
-        }
 
-        System.out.println("Enter elements of second matrix:");
-        for (int i = 0; i < r2; i++) {
-            for (int j = 0; j < c2; j++) {
-                matrix2[i][j] = sc.nextInt();
-            }
-        }
+            int[][] matrix1 = new int[r1][c1];
+            int[][] matrix2 = new int[r2][c2];
+            int[][] result = new int[r1][c2];
 
-        // Matrix multiplication
-        for (int i = 0; i < r1; i++) {
-            for (int j = 0; j < c2; j++) {
-                result[i][j] = 0;
-                for (int k = 0; k < c1; k++) {
-                    result[i][j] += matrix1[i][k] * matrix2[k][j];
+            System.out.println("Enter elements of first matrix:");
+            for (int i = 0; i < r1; i++) {
+                for (int j = 0; j < c1; j++) {
+                    matrix1[i][j] = sc.nextInt();
                 }
             }
-        }
 
-        System.out.println("Resultant Matrix:");
-        for (int i = 0; i < r1; i++) {
-            for (int j = 0; j < c2; j++) {
-                System.out.print(result[i][j] + " ");
+            System.out.println("Enter elements of second matrix:");
+            for (int i = 0; i < r2; i++) {
+                for (int j = 0; j < c2; j++) {
+                    matrix2[i][j] = sc.nextInt();
+                }
             }
-            System.out.println();
-        }
 
-        sc.close();
+            // Matrix multiplication
+            for (int i = 0; i < r1; i++) {
+                for (int j = 0; j < c2; j++) {
+                    result[i][j] = 0;
+                    for (int k = 0; k < c1; k++) {
+                        result[i][j] += matrix1[i][k] * matrix2[k][j];
+                    }
+                }
+            }
+
+            System.out.println("Resultant Matrix:");
+            for (int i = 0; i < r1; i++) {
+                for (int j = 0; j < c2; j++) {
+                    System.out.print(result[i][j] + " ");
+                }
+                System.out.println();
+            }
+
+            sc.close();
+        }
     }
 }

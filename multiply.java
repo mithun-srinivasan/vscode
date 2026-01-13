6ff6c973f5d209be29faa43a02451109e8d0403d
mithun-srinivasan <omsaimithun@gmail.com>
2026-01-13 23:34:21 +0530

    Add file handling and multiplication Java programs

---
+++ multiply.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+class mutiply
+{   
+    public static void main(String[] args) {
+
+        int a,b,c;
+        Scanner ob=new Scanner(System.in);
+        System.out.println("Enter 2 numbers");
+        a=ob.nextInt();
+        b=ob.nextInt();
+        c=a*b;
+        System.out.println("the multiplication is "+c);
+        
+        }
+}

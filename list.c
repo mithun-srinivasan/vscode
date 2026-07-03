c196126a49193570e4cee8ff8ce5d1c65dc74a5e
Mithun Srinivasan <omsaimithun@gmail.com>
2026-07-03 16:07:23 +0530

    refactor: enhance input handling and modularize list operations

--- list.c
+++ list.c
@@ -2,7 +2,44 @@
 
 #define MAX 100
 
-int main() {
+void clear_input(void)
+{
+    int ch;
+    while ((ch = getchar()) != '\n' && ch != EOF)
+    {
+    }
+}
+
+int read_int(const char *prompt, int *value)
+{
+    printf("%s", prompt);
+    if (scanf("%d", value) == 1)
+    {
+        return 1;
+    }
+    clear_input();
+    return 0;
+}
+
+void display_list(const int a[], int n)
+{
+    int i;
+    if (n == 0)
+    {
+        printf("List is empty\n");
+        return;
+    }
+
+    printf("List: ");
+    for (i = 0; i < n; i++)
+    {
+        printf("%d ", a[i]);
+    }
+    printf("\n");
+}
+
+int main(void)
+{
     int a[MAX];
     int n = 0;
     int choice;
@@ -14,51 +51,37 @@ int main() {
     while (1)
     {
         printf("\n1. Insert\n2. Search\n3. Delete\n4. Display\n5. Exit\n");
-        printf("Enter choice: ");
-        if (scanf("%d", &choice) != 1)
+        if (!read_int("Enter choice: ", &choice))
         {
             printf("Please enter a number only\n");
-            while (getchar() != '\n')
-            {
-            }
             continue;
         }
 
-        if (choice == 1)
+        switch (choice)
         {
+        case 1:
             if (n == MAX)
             {
                 printf("List is full\n");
+                break;
             }
-            else
-            {
-                printf("Enter number: ");
-                if (scanf("%d", &x) != 1)
-                {
-                    printf("Please enter a valid number\n");
-                    while (getchar() != '\n')
-                    {
-                    }
-                    continue;
-                }
-                a[n] = x;
-                n = n + 1;
-                printf("Inserted\n");
-            }
-        }
-        else if (choice == 2)
-        {
-            printf("Enter number to search: ");
-            if (scanf("%d", &x) != 1)
+            if (!read_int("Enter number: ", &x))
             {
                 printf("Please enter a valid number\n");
-                while (getchar() != '\n')
-                {
-                }
-                continue;
+                break;
+            }
+            a[n++] = x;
+            printf("Inserted\n");
+            break;
+
+        case 2:
+            if (!read_int("Enter number to search: ", &x))
+            {
+                printf("Please enter a valid number\n");
+                break;
             }
             found = 0;
-            for (i = 0; i < n; i = i + 1)
+            for (i = 0; i < n; i++)
             {
                 if (a[i] == x)
                 {
@@ -67,61 +90,41 @@ int main() {
                     break;
                 }
             }
-            if (found == 0)
+            if (!found)
             {
                 printf("Not found\n");
             }
-        }
-        else if (choice == 3)
-        {
-            printf("Enter position to delete: ");
-            if (scanf("%d", &pos) != 1)
+            break;
+
+        case 3:
+            if (!read_int("Enter position to delete: ", &pos))
             {
                 printf("Please enter a valid position\n");
-                while (getchar() != '\n')
-                {
-                }
-                continue;
+                break;
             }
             if (pos < 1 || pos > n)
             {
                 printf("Invalid position\n");
+                break;
             }
-            else
+            for (i = pos - 1; i < n - 1; i++)
             {
-                for (i = pos - 1; i < n - 1; i = i + 1)
-                {
-                    a[i] = a[i + 1];
-                }
-                n = n - 1;
-                printf("Deleted\n");
+                a[i] = a[i + 1];
             }
-        }
-        else if (choice == 4)
-        {
-            if (n == 0)
-            {
-                printf("List is empty\n");
-            }
-            else
-            {
-                printf("List: ");
-                for (i = 0; i < n; i = i + 1)
-                {
-                    printf("%d ", a[i]);
-                }
-                printf("\n");
-            }
-        }
-        else if (choice == 5)
-        {
+            n--;
+            printf("Deleted\n");
+            break;
+
+        case 4:
+            display_list(a, n);
+            break;
+
+        case 5:
+            return 0;
+
+        default:
+            printf("Invalid choice\n");
             break;
         }
-        else
-        {
-            printf("Invalid choice\n");
-        }
     }
-
-    return 0;
 }
